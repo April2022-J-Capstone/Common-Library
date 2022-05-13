5ad446ee55068dbf5ada687928559404a8de178f
Per Van Dyke <Per.VanDyke@smoothstack.com>
2022-05-12 20:57:27 -0400

    added new order find method
    Now you can enter a customer, and get all of their orders. Useful for order history.

--- src/main/java/com/smoothstack/common/repositories/OrderRepository.java
+++ src/main/java/com/smoothstack/common/repositories/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.smoothstack.common.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.smoothstack.common.models.Order;
+import com.smoothstack.common.models.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Optional<List<Order>> findAllByCustomer(User customer);
 }
