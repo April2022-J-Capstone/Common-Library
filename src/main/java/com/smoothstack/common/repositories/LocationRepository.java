20fda520942113e628131814fcb288dbcc6fb51c
Per Van Dyke <Per.VanDyke@smoothstack.com>
2022-06-17 09:08:36 -0400

    Find all locations with name
    This is to support the ability to look up locations when a new restaurant (or anything else with a location) is added, and find out if a location with the same name and address already exists.

--- src/main/java/com/smoothstack/common/repositories/LocationRepository.java
+++ src/main/java/com/smoothstack/common/repositories/LocationRepository.java
@@ -1,9 +1,14 @@
 package com.smoothstack.common.repositories;
 
 import com.smoothstack.common.models.Location;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
+    List<Optional<Location>> findAllByLocationName(String name);
 }
