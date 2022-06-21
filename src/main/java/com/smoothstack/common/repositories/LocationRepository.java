00dbf7d699d480b9754c35288e77daa4fdb4fe53
Per Van Dyke <Per.VanDyke@smoothstack.com>
2022-06-20 20:39:02 -0400

    Small updates to location entity and repo

--- src/main/java/com/smoothstack/common/repositories/LocationRepository.java
+++ src/main/java/com/smoothstack/common/repositories/LocationRepository.java
@@ -3,12 +3,11 @@ package com.smoothstack.common.repositories;
 import com.smoothstack.common.models.Location;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
-    Optional<List<Location>> findAllByLocationName(String name);
+    List<Location> findAllByLocationName(String name);
 }
